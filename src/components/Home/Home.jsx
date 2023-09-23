e60253495eea6ab9f00cf71571b88c3a53d8faab
smhabib99-99 <habibaust34@gmail.com>
2023-09-23 14:22:01 +0600

    successfully completed nested router with Outlet

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h2>This is the home component.</h2>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Home;
